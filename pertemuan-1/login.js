7a6c29435c017fc5261dae25bd9a3a6b34a3580f
Amar Ma'ruf <107825981+MrRuf04@users.noreply.github.com>
2022-10-26 17:12:15 +0700

    Add files via upload

---
+++ pertemuan-1/login.js
@@ -0,0 +1,21 @@
+document.getElementsByTagName("head")[0].appendChild
+(document.createElement("title"))
+document.getElementsByTagName("title")[0].innerText =
+"Tugas Mandiri 1"
+
+document.getElementsByTagName("body")[0].appendChild
+var username = document.createElement("input");
+username.setAttribute("type","text");
+username.setAttribute("placeholder","username");
+document.body.appendChild(username);
+
+document.getElementsByTagName("body")[0].appendChild
+var password = document.createElement("input");
+password.setAttribute("type","password");
+password.setAttribute("placeholder","password");
+document.body.appendChild(password);
+
+let body = document.getElementsByTagName("body")[0];
+let btn = document.createElement("button");
+body.appendChild(btn);
+btn.innerText = "Login"
